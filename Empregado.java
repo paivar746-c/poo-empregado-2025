22600343bef6650d897e66396f3c447155a9a34d
Marco Mazzei <marco.mazzei@maua.br>
2025-03-11 07:54:12 -0300

    Cálculo de salário

--- Empregado.java
+++ Empregado.java
@@ -34,4 +34,37 @@ public class Empregado {
     public void setIdade(int idade) {
         this.idade = idade;
     }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public int tipo() {
+        return this.tipo;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getSalario() {
+        return this.salario;
+    }
+
+    public void setComissao(double comissao) {
+        this.comissao = comissao;
+    }
+
+    public double getComissao() {
+        return this.comissao;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+    public double getBonus() {
+        return this.bonus;
+    }
+
 }
